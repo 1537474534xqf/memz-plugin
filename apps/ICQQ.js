48457c90e4c2681548c445c04840e5a545f50447
叶殇 <197728340@qq.com>
2025-01-07 14:42:02 +0800

    ✨ refactor(ICQQ): 修改音卡测试命令的参数说明逻辑
    - 将无效参数提示改为更友好的帮助信息回复
    - 优化了用户交互体验

--- apps/ICQQ.js
+++ apps/ICQQ.js
@@ -76,7 +76,7 @@ export class ICQQ extends plugin {
     if (!ICQQBotQQ) return e.reply('[memz-plugin] 未设置ICQQ Bot', true)
     const match = e.msg.match(/^#音卡测试(.*)$/i)
 
-    if (!match) { return e.reply('请提供有效的类型和其他参数,参数#音卡测试Appid,标题,内容,跳转链接,图片链接,群号', true) }
+    if (match[0] === '帮助') { return e.reply('参数#音卡测试Appid,标题,内容,跳转链接,图片链接,群号', true) }
 
     const customParams = match[1].split(',').map(param => param.trim())
 
